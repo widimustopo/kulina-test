b8612361cde1b906e6c9905f4206810589f9bdea
widimustopo <widhi.mustopo@gmail.com>
2021-06-14 17:50:52 +0700

    fixing logic

--- algorithm/turn_the_lamps_on/main.go
+++ algorithm/turn_the_lamps_on/main.go
@@ -1,37 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+type Lamps struct {
+	number int
+	turn   bool
+}
 
 func main() {
-	var multiTrip []int
-	numberA := 0
-	numberB := 100
-
-	var resTrip []int
-
 	var result int
+	var lamps []Lamps
+	number := 100
 
-	for numberA < numberB {
-
-		tries := (numberA + 1)
-		trip := numberB / tries
-		if tries <= 3 {
-			multiTrip = append(multiTrip, trip)
-
-			tries++
-		}
-
-		for _, i := range multiTrip {
-
-			resTrip = append(resTrip, i)
-		}
-
-		numberA++
+	//initial
+	for i := 1; i <= number; i++ {
+		lamps = append(lamps, Lamps{
+			number: i,
+			turn:   true,
+		})
 	}
 
-	for i := 0; i < numberB; i++ {
-		result += resTrip[i]
+	for i := 1; i < len(lamps); i++ {
+		secondTrip := math.Mod(float64(lamps[i].number), 2)
+		if secondTrip == 0 {
+			if lamps[i].turn {
+				lamps[i].turn = false
+			} else {
+				lamps[i].turn = true
+			}
+		}
+
+		thirdTrip := math.Mod(float64(lamps[i].number), 3)
+		if thirdTrip == 0 {
+			if lamps[i].turn {
+				lamps[i].turn = false
+			} else {
+				lamps[i].turn = true
+			}
+		}
+
+		allTrip := math.Mod(float64(lamps[i].number), 1)
+		if allTrip == 0 {
+			if lamps[i].turn {
+				lamps[i].turn = false
+			} else {
+				lamps[i].turn = true
+			}
+		}
 	}
 
-	fmt.Println(result)
+	for _, lamp := range lamps {
+		if lamp.turn {
+			result += 1
+		}
+	}
+
+	fmt.Println(" Lamps currently turn on are : ", result, " lamps")
 }
